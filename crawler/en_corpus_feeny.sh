787a2480c1713d486d4791529780db890d87d4ad
Saurabh Vyas <saurabhorange@msn.com>
2019-06-30 03:46:51 +0900

    added requirements.txt back, which i deleted accidently, fixed a small bug relating to python version in en feeny script

--- crawler/en_corpus_feeny.sh
+++ crawler/en_corpus_feeny.sh
@@ -7,7 +7,7 @@ filter_dir=$2
 declare -a arr=("boy meets world" "kim possible" )
 
 # num of videos to download per keyword
-num=3
+num=2
 
 # get length of an array
 arraylength=${#arr[@]}
@@ -23,7 +23,7 @@ do
           echo $i " / " ${arraylength} " : " ${arr[$i-1]}
           
           #youtube-dl -o "$target_dir/%(id)s%(title)s.%(ext)s" "https://www.youtube.com/results?sp=EgQIBCgB&q="${arr[$i-1]}"&p="$j --download-archive ./en-downloaded.txt --no-overwrites --restrict-filenames --youtube-skip-dash-manifest --max-downloads 3 --socket-timeout 20 -iwc --write-info-json -k --write-auto-sub --skip-download  --sub-format vtt --sub-lang en    --extract-audio --audio-format "wav"  --exec "python ./crawler/process.py {} '$filter_dir'"
-          youtube-dl -o "$target_dir/%(id)s%(title)s.%(ext)s" "ytsearch${num}:${arr[$i-1]}" --download-archive ./en-downloaded.txt --no-overwrites --restrict-filenames --youtube-skip-dash-manifest --max-downloads 20 --socket-timeout 20 -iwc --write-info-json -k --write-auto-sub   --sub-format vtt --sub-lang en    --extract-audio --audio-format "wav"  --exec "python ./crawler/process.py {} '$filter_dir'"
+          youtube-dl -o "$target_dir/%(id)s%(title)s.%(ext)s" "ytsearch${num}:${arr[$i-1]}" --download-archive ./en-downloaded.txt --no-overwrites --restrict-filenames --youtube-skip-dash-manifest --max-downloads 20 --socket-timeout 20 -iwc --write-info-json -k --write-auto-sub   --sub-format vtt --sub-lang en    --extract-audio --audio-format "wav"  --exec "python3 ./crawler/process.py {} '$filter_dir'"
   done
 done
 
